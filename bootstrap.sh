dc3d7f2bdc6fd2e2a474222866acba3b610b0a10
Kevin Ziegler <kevin.ziegler3@gmail.com>
2017-11-17 07:53:39 -0600

    Install Vim Plugins on bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,3 +10,10 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew doctor
 
 brew bundle
+
+# Setup vim plugins
+curl -fLo ~/.vim/autoload/plug.vim \
+     --create-dirs \
+     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim +PlugInstall +qall
