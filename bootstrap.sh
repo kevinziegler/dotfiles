e4c54d8eaf1bcb0c21f9154ef266d6cbaad82852
Kevin Ziegler <kevin.ziegler3@gmail.com>
2017-11-17 07:53:53 -0600

    Install spacemacs to emacs.d via bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,3 +17,6 @@ curl -fLo ~/.vim/autoload/plug.vim \
      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 vim +PlugInstall +qall
+
+# Setup spacemacs
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
