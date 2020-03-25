9bb3155437cf014653d5dd5b522b7c2992a51ee9
Kevin Ziegler <ziegler.kevin@heb.com>
2020-03-25 08:12:05 -0500

    [doom] Make org link abbrevs additive
    Don't overwrite the existing list, just add the ones I want.

--- doom/modules/private/heb/config.el
+++ doom/modules/private/heb/config.el
@@ -1,11 +1,15 @@
 ;;; heb/config.el -*- lexical-binding: t; -*-
 
 ;;; Code:
+;; (after! org
+;;   (setq org-link-abbrev-alist
+;;         '(("git.heb" . "https://git.heb.com/")
+;;           ("github" . "https://github.com/")
+;;           ("jira" . "https://hebecom.atlassian.net/browse/"))))
 (after! org
-  (setq org-link-abbrev-alist
-        '(("git.heb" . "https://git.heb.com/")
-          ("github" . "https://github.com/")
-          ("jira" . "https://hebecom.atlassian.net/browse/"))))
+       (map-put org-link-abbrev-alist "git.heb" "https://git.heb.com")
+       (map-put org-link-abbrev-alist "github" "https://github.com")
+       (map-put org-link-abbrev-alist "jira" "https://hebecom.atlassian.net/browse/"))
 
 (after! git-link
   (add-to-list 'git-link-remote-alist '("git\\.heb\\.com" git-link-gitlab)))
