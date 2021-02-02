f56d46c2d81b9bb032006d95c5d86f8382a84851
Kevin Ziegler <ziegler.kevin@heb.com>
2021-02-02 08:59:24 -0600

    [zsh] Clean up Aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,9 @@
-alias be="bundle exec"
-alias ec="emacsclient -nc"
-alias antibody-reload="antibody bundle < $ZSH_PLUGINS_SOURCE > $ZSH_PLUGINS_BUNDLE"
-alias ave="opav exec"
-alias av="opav"
-alias aws-vault="opav"
+alias be="bundle exec";
+alias ec="emacsclient -nc";
+alias antibody-reload="antibody bundle < $ZSH_PLUGINS_SOURCE > $ZSH_PLUGINS_BUNDLE";
+alias ave="opav exec";
+alias av="opav";
+alias aws-vault="opav";
+
+alias tf-browse="terraform state list | fzf --preview 'terraform state show {}'";
+alias tg-browse="terragrunt state list | fzf --preview 'terragrunt state show {}'";
