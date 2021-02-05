ccaad6af49cffd8fedb9063f51320b79e0a596c7
Kevin Ziegler <ziegler.kevin@heb.com>
2021-02-05 12:39:41 -0600

    [finicky] Add Finicky config

---
+++ finicky.js
@@ -0,0 +1,28 @@
+module.exports = {
+    defaultBrowser: "Firefox",
+    handlers: [
+        { match: /heb.zoom.us\/.+login\?from/, browser: "Google Chrome" },
+        { match: /zoom.us\/j\//, browser: "us.zoom.xos" },
+    ],
+
+    rewrite: [
+        {
+            match: /zoom.us\/s\//,
+            url: ({ url }) => ({
+                ...url,
+                pathname: url.pathname.replace(/^\/s\//, "/j/"),
+            }),
+        },
+/*
+        {
+            match: /zoom.us\/my\//,
+            url: ({ url }) => https.get(
+                url,
+                ({ res }) => res.statusCode === 302
+                    ? url
+                    : res.headers.location
+            ),
+        },
+ * */
+    ]
+}
