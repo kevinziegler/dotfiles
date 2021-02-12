59e5688bd3e803641641509537f4cfd3b17dd484
Kevin Ziegler <ziegler.kevin@heb.com>
2021-02-12 16:53:54 -0600

    [emacs] Remove 'custom' section of config.el

--- doom/doom.d/config.el
+++ doom/doom.d/config.el
@@ -62,19 +62,3 @@
 
 (add-hook 'sh-mode-hook #'kdz/set-zshrc-sh-shell)
 ;;(add-hook 'markdown-mode-hook #'kdz/writing-fill-column)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
-   '((eval add-hook 'java-mode-hook
-           (lambda nil
-             (setq c-basic-offset 4 tab-width 4 indent-tabs-mode t))))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
