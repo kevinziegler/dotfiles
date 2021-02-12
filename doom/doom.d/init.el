c7bfbbb9e887b26e5330b71e1d01a5ba1bd62541
Kevin Ziegler <ziegler.kevin@heb.com>
2021-02-12 16:53:32 -0600

    [emacs] Show icons in Ivy

--- doom/doom.d/init.el
+++ doom/doom.d/init.el
@@ -22,7 +22,7 @@
        (company +childframe)           ; the ultimate code completion backend
        ;; (helm +fuzzy +childframe) ;; +childframe +fuzzy)              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +fuzzy +childframe)                ; a search engine for love and life
+       (ivy +fuzzy +childframe +icons)                ; a search engine for love and life
 
        :ui
        deft              ; notational velocity for Emacs
