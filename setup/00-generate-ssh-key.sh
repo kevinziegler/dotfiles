bd58ccbbc0d6b379a7cf8c8f765ab4cb1a745781
Kevin Ziegler <ziegler.kevin@heb.com>
2021-08-04 10:28:25 -0500

    [boostrap] Backup existing ssh key if it exists

--- setup/00-generate-ssh-key.sh
+++ setup/00-generate-ssh-key.sh
@@ -3,7 +3,13 @@
 set -euf -o pipefail;
 
 echo "[$(basename $0)] Generating an $SSH_KEY_TYPE SSH key at $SSH_KEY";
-# TODO Backup an existing SSH key at this location
+
+if [[ -f "$SSH_KEY" ]]; then
+    SSH_KEY_BACKUP="$SSH_KEY.bak";
+    echo "Existing SSH Key found at $SSH_KEY.  Backing up to $SSH_KEY_BACKUP";
+    mv "$SSH_KEY" "$SSH_KEY_BACKUP";
+fi
+
 ssh-keygen -t "$SSH_KEY_TYPE" -f "$SSH_KEY" -N "";
 
 exit 0;
