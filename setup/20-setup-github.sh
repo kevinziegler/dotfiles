a164eb26c6015321824ffe8dd2b057c8b917a07b
Kevin Ziegler <ziegler.kevin@heb.com>
2021-08-04 10:29:06 -0500

    [bootstrap] Update hub setup to use yq v4
    The newest version of yq has made substantial changes to its interface;
    this is just accounting for those.

--- setup/20-setup-github.sh
+++ setup/20-setup-github.sh
@@ -6,13 +6,15 @@ source "$OP_SESSION";
 GH_OP_ITEM="github";
 GH_HUB_CONFIG="$HOME/.config/hub";
 GH_SSHKEY="$(ssh-keygen -y -f "$SSH_KEY")";
-TOKEN=$(op get item "$GH_OP_ITEM" --fields "Hub Access Token");
-USER=$(op get item "$GH_OP_ITEM" --fields "username");
+GITHUB_TOKEN=$(op get item "$GH_OP_ITEM" --fields "Hub Access Token");
+GITHUB_USER=$(op get item "$GH_OP_ITEM" --fields "username");
 
 echo "Setting up hub client for Github...";
-yq write "$GH_HUB_CONFIG" "[github.com][0].username" "$GITHUB_USER";
-yq write "$GH_HUB_CONFIG" "[github.com][0].oauth_token" "$GITHUB_TOKEN";
-yq write "$GH_HUB_CONFIG" "[github.com][0].protocol" "https";
+
+touch $GH_HUB_CONFIG;
+yq eval -i '."github.com"[0].username = strenv(GITHUB_USER)' $GH_HUB_CONFIG;
+yq eval -i '."github.com"[0].oauth_token = strenv(GITHUB_TOKEN)' $GH_HUB_CONFIG;
+yq eval -i '."github.com"[0].protocol = "https"' $GH_HUB_CONFIG;
 
 echo "Adding Github SSH key...";
 hub api user/keys --field title="$GH_SSHKEY_NAME" --field key="$GH_SSHKEY";
