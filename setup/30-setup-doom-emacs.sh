fba3636c372ff1d2ecebbb66f0639b33d22d9ef2
Kevin Ziegler <kevinziegler@Kevins-MBP.asgard>
2021-08-30 14:48:26 -0500

    [setup] Install all-the-icons for emacs

--- setup/30-setup-doom-emacs.sh
+++ setup/30-setup-doom-emacs.sh
@@ -9,5 +9,6 @@ hub clone "$GITHUB_DOOM_D_REPO" "$HOME/.doom.d";
 hub clone "$GITHUB_DOOM_REPO" "$HOME/.emacs.d";
 
 "$HOME/.emacs.d/bin/doom" install;
+emacs --eval "(all-the-icons-install-fonts t)" --kill;
 
 exit 0;
