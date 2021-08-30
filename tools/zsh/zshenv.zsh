7ade65e83be4609ac6213732d911781b3e995582
Kevin Ziegler <kevinziegler@Kevins-MBP.asgard>
2021-08-30 14:30:57 -0500

    [zsh] Reference correct plugins file

--- tools/zsh/zshenv.zsh
+++ tools/zsh/zshenv.zsh
@@ -1,5 +1,5 @@
 export DOTFILES="$HOME/.dotfiles";
-export ZSH_PLUGINS_SOURCE=$HOME/.dotfiles/zsh/plugins.txt;
+export ZSH_PLUGINS_SOURCE=$HOME/.dotfiles/tools/zsh/plugins.txt;
 export ZSH_PLUGINS_BUNDLE=$HOME/.plugins.zsh;
 export TERM="xterm-256color";
 export FZF_JELLYBEANS_OPTS="--color fg:-1,bg:-1,hl:230,fg+:3,bg+:233,hl+:229 --color info:150,prompt:110,spinner:150,pointer:167,marker:174";
