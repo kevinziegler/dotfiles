7ff3b2f1666047149927a0157b3838225853a754
Kevin Ziegler <kevin@bestow.com>
2021-10-28 10:52:10 -0500

    [zsh] Fix EDITOR variable

--- tools/zsh/zshenv.zsh
+++ tools/zsh/zshenv.zsh
@@ -4,7 +4,7 @@ export ZSH_PLUGINS_BUNDLE=$HOME/.plugins.zsh;
 export TERM="xterm-256color";
 export FZF_JELLYBEANS_OPTS="--color fg:-1,bg:-1,hl:230,fg+:3,bg+:233,hl+:229 --color info:150,prompt:110,spinner:150,pointer:167,marker:174";
 export FZF_DEFAULT_OPTS="--prompt='➜ ' --pointer='‣' --marker='•' $FZF_JELLYBEANS_OPTS";
-export EDITOR="emacs-client-editor";
+export EDITOR="ec";
 export ALTERNATE_EDITOR="vim";
 export CLICOLOR=1;
 export PSPG="--style=22";
