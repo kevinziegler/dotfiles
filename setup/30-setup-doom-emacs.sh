ff778cecbb3ed9b1c521bfa4318707dda9c3465b
Kevin Ziegler <kevin@bestow.com>
2022-04-14 15:32:17 -0500

    [setup] Clone doom configs via Git
    The `hub clone` fails on the `doom-emacs` repo, complaining about a
    now-removed unauthenticated API.  This seems to get around that issue.

--- setup/30-setup-doom-emacs.sh
+++ setup/30-setup-doom-emacs.sh
@@ -2,11 +2,11 @@
 
 set -euf -o pipefail;
 
-GITHUB_DOOM_D_REPO="kevinziegler/doom.d";
-GITHUB_DOOM_REPO="hlissner/doom-emacs";
+GITHUB_DOOM_D_REPO="git@github.com:kevinziegler/doom.d";
+GITHUB_DOOM_REPO="git@github.com:hlissner/doom-emacs";
 
-hub clone "$GITHUB_DOOM_D_REPO" "$HOME/.doom.d";
-hub clone "$GITHUB_DOOM_REPO" "$HOME/.emacs.d";
+git clone "$GITHUB_DOOM_D_REPO" "$HOME/.doom.d";
+git clone "$GITHUB_DOOM_REPO" "$HOME/.emacs.d";
 
 "$HOME/.emacs.d/bin/doom" install;
 emacs --eval "(all-the-icons-install-fonts t)" --kill;
