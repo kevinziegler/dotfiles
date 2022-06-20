d99697d5f0242c45107d9d7978175c939ba26c7c
Kevin Ziegler <kevinziegler@Balder.asgard>
2022-06-20 12:09:47 -0500

    [bootstrap] Use dotdrop for system setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,36 +1,71 @@
 #! /usr/bin/env bash
 
-set -euf -o pipefail;
-
-# Export config variables needed by setup scripts
-export SSH_KEY_TYPE="ed25519";
-export SSH_KEY="$HOME/.ssh/id_$SSH_KEY_TYPE";
-export DOTFILES="$HOME/.dotfiles";
-export OP_SESSION=$(mktemp);
-
 # Add /usr/local/bin and /opt/homebrew/bin to the path so that anything
 # installed via homebrew is available to setup scripts
 export PATH=$PATH:/usr/local/bin:/opt/homebrew/bin;
 
-# Config values only needed for bootstrapping
+set -euf -o pipefail;
+
+DOTFILES="$HOME/.dotfiles";
 DOTFILES_REPO="kevinziegler/dotfiles.git";
-DOTFILES_REPO_HTTPS="https://github.com/$DOTFILES_REPO";
-DOTFILES_REPO_SSH="git@github.com:$DOTFILES_REPO";
+DOTFILES_REPO_HTTPS="https://github.com/${DOTFILES_REPO}";
+DOTFILES_REPO_SSH="git@github.com:${DOTFILES_REPO}";
+BREW_URL="https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh";
+op_setup_file="onepassword-setup-link.txt";
+op_setup_file_search_path="/Volumes/ESD-USB/";
+op_setup_file_path=$(find "${op_setup_file_search_path}" -name "${op_setup_file}");
 
 # Clone repository into ~/.dotfiles via HTTPS
-git clone "$DOTFILES_REPO_HTTPS" "$DOTFILES";
+git clone --recurse-submodules "${DOTFILES_REPO_HTTPS}" "${DOTFILES}";
 
 # Update remote for dotfiles repo to use SSH
 git -C $DOTFILES remote remove origin;
 git -C $DOTFILES remote add origin "$DOTFILES_REPO_SSH";
 
-# Run setup scripts
-echo "Bootstrapping complete.  Running setups scripts in $DOTFILES/setup";
-for script in $DOTFILES/setup/*.sh; do
-    echo "[$script] Starting";
-    bash $script;
-    echo "[$script] Finished";
-done
 
-echo "Setup is finished!";
+if ! which brew &> /dev/null; then
+	echo "Homebrew not found...Installing";
+	/bin/bash -c "$(curl -fsSl $BREW_URL)";
+fi
+
+"$DOTFILES/dotdrop/dotdrop.sh" -c "$DOTFILES/dotdrop.bootstrap.yaml" -P bootstrap;
+
+# First, setup 1Password so that we can access personal credentials
+echo "Launching 1Password...";
+open -a /Applications/1Password.app;
+
+if [[ -z "${op_setup_file_path}" ]]; then
+    echo "Could not find 1Password setup file.  Set up 1Password manually.";
+else
+    echo "1Password setup file found! Opening magic link.";
+    open "$(cat ${op_setup_file_path})";
+fi
+
+echo "Launching Firefox, setting it as the default browser...";
+/Applications/Firefox.app/Contents/MacOS/firefox \
+    --setDefaultBrowser \
+    "https://accounts.firefox.com/signin";
+
+echo "Sign into your Mozilla Account.";
+sleep 3;
+echo "Press <ENTER> to continue when Mozilla setup is complete";
+read;
+
+echo "Sign into your Google Account.";
+open "https://accounts.google.com";
+sleep 3;
+echo "Press <ENTER> to continue when Google Account setup is complete";
+read;
+
+echo "Launching/Signing into Google Drive.  Follow prompts to complete setup";
+open -a "/Applications/Google Drive.app";
+sleep 3;
+echo "Press <ENTER> to continue when Google Drive setup is complete";
+read;
+
+echo "Manual Setup Steps Complete";
+
+
+"$DOTFILES/dotdrop/dotdrop.sh" -c "$DOTFILES/dotdrop.yaml" -P dev_system;
+
 exit 0;
