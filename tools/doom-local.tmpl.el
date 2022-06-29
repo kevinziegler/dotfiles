7bc9c74fda04dd962bc5dcd4964a1be421a3dd98
Kevin Ziegler <kziegler@ojolabs.com>
2022-06-29 12:07:18 -0500

    [emacs] Add default project path to local template

--- tools/doom-local.tmpl.el
+++ tools/doom-local.tmpl.el
@@ -1,3 +1,4 @@
 (setq user-full-name "Kevin Ziegler"
       user-mail-address "{{@@ system_email @@}}"
-      doom-theme 'doom-old-hope)
+      doom-theme 'doom-old-hope
+      projectile-project-search-path "~/dev")
