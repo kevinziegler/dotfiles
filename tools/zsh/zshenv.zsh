2d74907a7760e725a424e7bcb9e420597b107ae2
Kevin Ziegler <kziegler@ojolabs.com>
2022-12-19 11:02:04 -0600

    Add rounded border to default FZF opts

--- tools/zsh/zshenv.zsh
+++ tools/zsh/zshenv.zsh
@@ -3,7 +3,7 @@ export ZSH_PLUGINS_SOURCE=$HOME/.dotfiles/tools/zsh/plugins.txt;
 export ZSH_PLUGINS_BUNDLE=$HOME/.plugins.zsh;
 export TERM="xterm-256color";
 export FZF_JELLYBEANS_OPTS="--color fg:-1,bg:-1,hl:230,fg+:3,bg+:233,hl+:229 --color info:150,prompt:110,spinner:150,pointer:167,marker:174";
-export FZF_DEFAULT_OPTS="--prompt='➜ ' --pointer='‣' --marker='•' $FZF_JELLYBEANS_OPTS";
+export FZF_DEFAULT_OPTS="--prompt='➜ ' --pointer='‣' --marker='•' --border='rounded' $FZF_JELLYBEANS_OPTS";
 export EDITOR="ec";
 export ALTERNATE_EDITOR="vim";
 export CLICOLOR=1;
