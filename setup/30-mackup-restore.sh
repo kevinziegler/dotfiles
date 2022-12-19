79b596834bfa45fc31a8e537eefaa5c1a87f8789
Kevin Ziegler <kziegler@ojolabs.com>
2022-12-19 10:56:56 -0600

    Remove old setup scripts

--- setup/30-mackup-restore.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-set -euf -o pipefail;
-# FIXME This won't work until Google Drive is set up
-# mackup restore;
-
-exit 0;
--- setup/30-setup-zsh-plugins.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euf -o pipefail;
-
-antibody bundle < "$DOTFILES/tools/zsh/plugins.txt" > "$HOME/.plugins.zsh";
-
-exit 0;
