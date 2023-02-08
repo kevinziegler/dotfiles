b4c04c846e6945f4c5582821789716f08045900c
Kevin Ziegler <kziegler@ojolabs.com>
2023-02-08 14:38:59 -0600

    Add short aliases for common base commands

--- tools/zsh/aliases.zsh
+++ tools/zsh/aliases.zsh
@@ -10,10 +10,16 @@ alias listening-ports="sudo lsof -PiTCP -sTCP:LISTEN";
 alias tf-browse="terraform state list | fzf --preview 'terraform state show {}'";
 alias tg-browse="terragrunt state list | fzf --preview 'terragrunt state show {}'";
 
+
+alias d="docker";
+alias g="git";
+alias k="kubectl";
+
+alias nuke="rm -rf";
+alias csv="pspg --csv --csv-header on";
+alias de="docker-explore";
+alias gb="gitbatch -r 5";
+
 # Overriding unix utilities
 alias ls="exa";
 alias cat="bat";
-
-alias csv="pspg --csv --csv-header on";
-
-alias nuke="rm -rf";
