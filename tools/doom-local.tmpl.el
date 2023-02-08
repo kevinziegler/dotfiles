b9beecaf84b60cd17de462b71249c8307ce735eb
Kevin Ziegler <kziegler@ojolabs.com>
2023-02-08 14:40:24 -0600

    Update local.el template

--- tools/doom-local.tmpl.el
+++ tools/doom-local.tmpl.el
@@ -1,4 +1,7 @@
 (setq user-full-name "Kevin Ziegler"
       user-mail-address "{{@@ system_email @@}}"
       doom-theme 'doom-old-hope
+      jiralib-url nil
+      jira-host nil
+      hosted-gitlab-host nil
       projectile-project-search-path '("~/dev"))
